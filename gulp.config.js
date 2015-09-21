10c6735addacf5ab3cf7c6ab5832a1d6aa922c4b
unknown <isavin@P5RO-PORT468.pentalog.com>
2015-09-21 18:18:28 +0300

    aaa

--- gulp.config.js
+++ gulp.config.js
@@ -2,7 +2,9 @@ module.exports = function() {
   var config = {
     alljs: [
       'test config'
-    ]
+    ],
+    sass: './app/',
+    css: './dist/css'
   };
 
   return config;
--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,32 @@ var gulp = require('gulp');
 var config = require('./gulp.config')();
 var $ = require('gulp-load-plugins')({ lazy: true });
 
-gulp.task('hello', function() {
-  var helloLogTemplate = "***************************************"
-                       + " this is my hello"
-                       + "****************************************";
+var logTemplate = "*********************************************"
+                + "*********************************************";
 
-  log(helloLogTemplate);
+gulp.task('hello', function() {
+  log(logTemplate);
 
   console.log(config.alljs);
   // TODO: Add gulp print
 });
 
+gulp.task('sass', ['hello'], function () {
+  log(logTemplate);
 
+  gulp.src(config.scss + '/**/*.scss')
+    .pipe($.sass().on('error', $.sass.logError))
+    .pipe(gulp.dest(config.css));
+});
 
 function log(msg) {
   if (typeof(msg) === 'object') {
     for (var item in msg) {
       if (msg.hasOwnProperty(item)) {
-        $.util.log($.util.colors.magenta(msg[item]));
+        $.util.log($.util.colors.yellow(msg[item]));
       }
     }
   } else {
-    $.util.log($.util.colors.magenta(msg));
+    $.util.log($.util.colors.yellow(msg));
   }
 }
